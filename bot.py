e7961e2a7462e71f319fc2dd4f8536a11a056497
Kshitish Deshpande <43273771+ksdfg@users.noreply.github.com>
2020-04-15 21:20:17 +0530

    remove useless functionalities

--- bot.py
+++ bot.py
@@ -26,8 +26,6 @@ else:
 
 bot = telebot.AsyncTeleBot(data['bot-token'])  # Create bot object
 
-ids = dd(lambda: [])  # List of ids to send message to
-
 
 # Decorator for authorizing ids that send certain commands
 def needs_authorization(func):
@@ -42,37 +40,12 @@ def needs_authorization(func):
 
 def normalise(txt):
     return re.sub('^/\w+[ ,\n]', '', txt)  # To remove the /command@botname from message.text
-
-
-def dogbin(content):
-    # Save names of who all are gonna get messages in dogbin
-    return 'https://del.dog/{}'.format(json.loads(requests.post("https://del.dog/documents",
-                                                                content).content.decode())['key'])
-
-
-def getData(url, token, ids):
-    names_list = []  # List of all names
-    numbers_list = []  # List of all numbers
-
-    # Get data from our API
-    api_data = json.loads(requests.get(url, headers={'Authorization': token}).text)
-
-    if ids == 'all':
-        ids = list(map(lambda x: x['id'], api_data))
-
-    # Add names and numbers to respective lists
-    for user in api_data:
-        if int(user['id']) in ids:
-            names_list.append(user['name'])
-            numbers_list.append(user['phone'].split('|')[-1])
-
-    return names_list, numbers_list
     
     
 # Just to piss of Pranav
 @bot.message_handler(func=lambda message: str(message.from_user.id) == '893696358')
-def piss_of_bakre(message):
-    bot.reply_to(message, "ksdfg is a bastard")
+def piss_off_bakre(message):
+    bot.reply_to(message, "curse you gay toothpick")
 
 
 # No meow ksdfg
@@ -103,20 +76,6 @@ def echo(message):
 @bot.message_handler(commands=['coolcoolcoolcoolcool'])
 def peralta(message):
     bot.reply_to(message, 'nodoubtnodoubtnodoubtnodoubtnodoubt')
-
-
-# Responds to caller with the current api url from which data is requested
-@bot.message_handler(commands=['showurl'])
-def showURL(message):
-    bot.reply_to(message, data['url'])
-
-
-# Responds to caller with the current list of names to whom message is to be sent
-@bot.message_handler(commands=['showlist'])
-def showlist(message):
-    names, _ = getData(data['url'], data['api-token'], ids['nyan'])
-    bot.reply_to(message, 'The list of names to whom the message will be sent can be found at\n' +
-                 dogbin('\n'.join(names)))
 
 
 # Start ze bot
